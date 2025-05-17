5dbec35a9b2d538aaa91e578c4f54c543d0983f7
Uber-hub <60351073+Ubermensch-hub@users.noreply.github.com>
2025-05-17 17:19:16 +0600

    Initial

---
+++ Data/Report_Handler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+using System.Windows;
+using Microsoft.Win32;
+using System.IO;
+
+namespace PMBusInterface.Data
+{
+    static class Report_Handler
+    {
+        private static readonly string _defaultFileName = @"\Report";
+        private static readonly string _defaultFileExt = ".xlsx";
+
+        public static void CreateReport(string filePath)
+        {
+            ExcelPackage.License.SetNonCommercialOrganization("BitBlaze");
+
+            string fullPath = filePath + _defaultFileName + _defaultFileExt;
+
+            FileInfo newFile = new FileInfo(fullPath);
+            if (newFile.Exists)
+            {
+                newFile.Delete();
+                newFile = new FileInfo(fullPath);
+            }
+            using (ExcelPackage package = new ExcelPackage(newFile))
+            {
+                // add a new worksheet to the empty workbook
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Inventory");
+
+                package.Save();
+                MessageBox.Show("File successfully saved. Path: " + fullPath);
+            }
+        }
+
+        public static string GetDefaultFilePath()
+        {
+            string userName = Environment.UserName;
+            return @"C:\Users\" + userName + @"\Desktop";
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.IO.Packaging;
+using System.IO.Ports;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PMBusInterface.Data;
 
 namespace PMBusInterface
 {
@@ -16,9 +21,92 @@ namespace PMBusInterface
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string AvalailablePorts { get; private set; }
+
+        public string ScanString { get; private set; }
+
+        public string PSUString { get; private set; }
+
+        public string FRUString { get; private set; }
+
+        private readonly SerialPort _serialService;
+       
+
         public MainWindow()
         {
             InitializeComponent();
+
+            AvalailablePorts = "";
+            ScanString = "";
+            PSUString = "";
+            FRUString = "";
+
+            _serialService = new SerialPort();
+
+            
+
+            FindAvailablePorts();
+        }
+
+        private void FindAvailablePorts()
+        {
+            // here you should find all available ports
+            available_ports.ItemsSource = new COM_Port[]
+            {
+                new COM_Port {Name = "Port 1"},
+                new COM_Port {Name = "Port 2"},
+            };
+        }
+
+        public void ScanPort(object sender, RoutedEventArgs e)
+        {
+            //_serialService.Write("SCAN");
+
+            // change scan string (ScanString)
+            if(available_ports.SelectedItem is COM_Port port)
+            {
+                addresses.Text = port.Name;
+            }
+        }
+
+        public void CheckFRU(object sender, RoutedEventArgs e)
+        {
+            // read uint from input field (read addres from text box)
+            // change scan string (FRUString)
+            FRUoutput.Text = fru_input.Text;
+        }
+
+        public void CheckPSU(object sender, RoutedEventArgs e)
+        {
+            // read uint from input field (read addres from text box)
+            // change scan string (PSUString)
+            PSUoutput.Text = psu_input.Text;
+        }
+
+        public void ExportToExcel(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Report_Handler.CreateReport(output_path.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error - " + ex);
+            }
+        }
+
+        public void ChooseOutputFolder(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new OpenFolderDialog
+            {
+                // todo 
+            };
+
+            if (folderDialog.ShowDialog() == true)
+            {
+                output_path.Text = folderDialog.FolderName;
+                // Do something with the result
+            }
         }
     }
 }
